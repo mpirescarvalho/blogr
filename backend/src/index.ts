488e137f75c60cee19954af808d1e8263fab2d9a
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2021-07-27 00:58:32 -0300

    (backend): add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
-import 'dotenv/config'
+import 'dotenv/config';
 
-import express from 'express'
-import routes from './routes'
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 const PORT = process.env.PORT || 3333;
